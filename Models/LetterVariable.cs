930aeef93ceccdfbc223cc8a23a125cb9fa7ad98
Scott Hutley <scotthutley1@comcast.net>
2021-09-27 10:14:25 -0700

    Add .gitignore
    Co-authored by: Nathan Fletcher <nathan.fletcher@gmail.com>

---
+++ Models/LetterVariable.cs
@@ -0,0 +1,19 @@
+namespace FriendLetter.Models
+{
+  public class LetterVariable
+  {
+    public string Recipient { get; set; }
+    public string Sender { get; set; }
+    public string Destination { get; set; }
+    public string DreamDestination { get; set; }
+
+
+    // public LetterVariable(string recipient, string sender)
+    // {
+    //   Recipient = recipient;
+    //   Sender = sender;
+    // }
+
+
+  }
+}
